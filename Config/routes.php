c042cdadf800eeeae37f70b2d4f97d567c0455dd
kasper <sebb@sebb.dk>
2015-06-24 17:43:48 +0200

    routefix

--- Config/routes.php
+++ Config/routes.php
@@ -16,7 +16,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'bookingtypes', 'action' => 'index'));
+	Router::connect('/', array('controller' => 'booking_types', 'action' => 'index'));
 	Router::connect('/admin', array('controller' => 'users', 'action' => 'login', 'prefix' => 'admin'));
 	Router::connect('/users/login', array('controller' => 'users', 'action' => 'admin_login'));
 
