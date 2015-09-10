365039093ec500001290d70c00a5fde7577d86e8
kasper <sebb@sebb.dk>
2015-09-10 13:43:19 +0200

    fixed booking times not showing up

--- Controller/OpenTimesController.php
+++ Controller/OpenTimesController.php
@@ -3,9 +3,15 @@
  * @Author: sebb
  * @Date:   2015-07-01 18:40:11
  * @Last Modified by:   sebb
- * @Last Modified time: 2015-07-01 18:40:19
+ * @Last Modified time: 2015-09-10 13:41:49
  */
 
 class OpenTimesController extends AppController {
 	
+	public $paginate = [
+		'order' => 'start desc',
+		'limit' => 1000,
+		'maxLimit' => 1000
+	];
+
 }
